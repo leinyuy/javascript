11ffd54a26296ad56e174352bbcdaaed72231799
Dre Part <57998158+leinyuy@users.noreply.github.com>
2023-07-11 11:24:43 +0100

    just finished increment

---
+++ introduction.js
@@ -0,0 +1,33 @@
+// // variable declarations
+// var myVariable = "hello world";
+// myVariableName = "Good morning";
+// const variable = 10;
+// var initialVariable= null;
+
+
+// console.log(myVariable);
+// console.log(typeof myVariable);
+// console.log(variable);
+// console.log(typeof initialVariable);
+
+/* operators in javascript */
+
+// var a=2, b=10;
+// console.log(a+b);
+
+// console.log(a-b);
+
+// console.log(a*b);
+
+// console.log(a/b);
+
+// console.log(a%b);
+
+/*incrementation and decrementation*/ 
+// var increment=2, decrement =10;
+// console.log(increment, decrement)
+
+
+// temp = increment++ +2;
+// console.log(temp)
+
